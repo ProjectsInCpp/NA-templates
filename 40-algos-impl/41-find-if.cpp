d53913c9e0fe769c4db814a1ed39465dd9d47f66
Larter <m35sosnicki@gmail.com>
2017-07-24 12:07:09 +0200

    Update 41-find-if.cpp

--- 40-algos-impl/41-find-if.cpp
+++ 40-algos-impl/41-find-if.cpp
@@ -5,6 +5,7 @@
 TEST_CASE("successful find_if", "[algorithm]")
 {
     std::vector<int> data{ 1, 2, 3, 4 };
+    CHECK(training::find_if(data.end(), data.end(), [](auto && ){ return false; }) == data.end());
     CHECK(training::find_if(data.begin(), data.end(), [](auto && elem){ return elem == 4; }) == data.begin() + 3);
     CHECK(training::find_if(data.begin(), data.end(), [](auto && elem){ return elem == 1; }) == data.begin());
     CHECK(training::find_if(data.begin(), data.end(), [](auto && elem){ return elem == 5; }) == data.end());
